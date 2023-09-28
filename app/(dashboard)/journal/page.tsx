cd179ad6d05f617b6c9eeadab209d409dd6e4e2e
Masaya Kawai <mas@42zz.net>
2023-09-28 16:17:49 +0900

    add editor function

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,8 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import Link from 'next/link'
 
 const getEntries = async () => {
   const user = await getUserByClerkID()
@@ -17,8 +20,19 @@ const getEntries = async () => {
 
 const JournalPage = async () => {
   const entries = await getEntries()
-  console.log(entries)
-  return <div>journal</div>
+  return (
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id} entry={entry}>
+            <EntryCard entry={entry}/>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default JournalPage
